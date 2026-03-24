b5f29763fbbd8404aeb42c4b3a97f4fc9e81bf01
Marshall Whittaker <marshall@oxasploits.com>
2026-03-24 15:29:03 -0400

    Updated for os detection.

--- Main/Frontend/src/back-comm.js
+++ Main/Frontend/src/back-comm.js
@@ -1,8 +1,16 @@
 const { ipcMain } = require("electron");
 const { exec } = require("child_process");
+const os = require("os");
+
+const platform = os.platform();
 ipcMain.handle("run-backend-command", async (event, filename) => {
   console.log(`Received pcap: ${filename}`);
-  const command = `/usr/lib/packetsnitch/resources/backend/snitch "${filename}" -a -o /tmp/testcases`;
+  if (platform === "win32") {
+    command = `snitch.exe "${filename}" -a -o C:\\tmp\\testcases`;
+  }
+  if (platform === "linux") {
+    command = `/usr/lib/packetsnitch/resources/backend/snitch "${filename}" -a -o /tmp/testcases`;
+  }
   //const command = `sh /usr/lib/packetsnitch/resources/backend/loader.sh`;
   console.log("Command to run:", command);
   return new Promise((resolve, reject) => {
